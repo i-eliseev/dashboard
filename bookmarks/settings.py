885cbe91f2cd88796b80c608a98fb00c98e9d6a3
followthegoose <googoosik88@protonmail.com>
2020-02-26 10:18:05 +0300

    Ready for deployment to heroku.

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -167,5 +167,24 @@ EMAIL_HOST_PASSWORD = 'zel4630299'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+#Настройки Хероку
+
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    #Разрешены все заголовки хостов
+    ALLOWED_HOSTS = ['*']
+
+    #Конфигурация статических ресурсов
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
 
 
--- bookmarks/wsgi.py
+++ bookmarks/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import os
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookmarks.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
